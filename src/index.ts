4c796767dd948e74dfadbbbe15330933f56af839
Dan Johnson <dan@try.be>
2023-07-12 20:23:33 +0100

    feat: v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -74,6 +74,7 @@ export class CloudKitJs {
                 record: {
                     recordType: record.recordType,
                     fields: record.fields,
+                    recordName: record.recordName,
                 }
             }))
         })
