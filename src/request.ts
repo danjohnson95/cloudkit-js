1f86df172370cc50b3c65b782ee91a55584ff5d4
Dan Johnson <dan@try.be>
2024-11-11 23:22:55 +0000

    fix: support gzipped response

--- src/request.ts
+++ src/request.ts
@@ -1,5 +1,6 @@
 import https from "https"
 import { ISignService } from "./types";
+import zlib from "zlib";
 
 export class RequestService {
     protected readonly hostname = "api.apple-cloudkit.com";
@@ -24,15 +25,54 @@ export class RequestService {
 
         return new Promise((resolve, reject) => {
             const request = https.request(requestOptions, function(response) {
-                let responseBody = ""
+                const responseBody: Buffer[] = [];
 
-                response.on("data", function(chunk) {
-                    responseBody += chunk.toString("utf8")
-                })
+                response.on("data", function (chunk) {
+                    responseBody.push(chunk)
+                });
+                response.on("end", function () {
+                    let buffer = Buffer.concat(responseBody);
+                    const encoding = response.headers['content-encoding'];
+                    let responseObject = {}
 
-                response.on("end", function() {
-                    resolve(JSON.parse(responseBody))
-                })
+                    if (encoding === 'gzip') {
+                        zlib.gunzip(buffer, (err, decoded) => {
+                            if (err) {
+                                return reject(err);
+                            }
+
+                            try {
+                                responseObject = JSON.parse(decoded.toString('utf8'));
+                            } catch (e) {
+                                return reject(e);
+                            }
+
+                            resolve(responseObject as T);
+                        });
+                    } else if (encoding === 'deflate') {
+                        zlib.inflate(buffer, (err, decoded) => {
+                            if (err) {
+                                return reject(err);
+                            }
+
+                            try {
+                                responseObject = JSON.parse(decoded.toString('utf8'));
+                            } catch (e) {
+                                return reject(e);
+                            }
+
+                            resolve(responseObject as T);
+                        });
+                    } else {
+                        try {
+                            responseObject = JSON.parse(buffer.toString('utf8'));
+                        } catch (e) {
+                            return reject(e);
+                        }
+
+                        resolve(responseObject as T);
+                    }
+                });
             })
 
             request.on("error", function(err) {
